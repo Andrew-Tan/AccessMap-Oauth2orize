c9f2db534a0b05f5d619be2b37692cab15c8bcb2
Andrew Tan <tan95@cs.washington.edu>
2017-06-23 15:28:34 -0700

    support json datatype for scope

--- authorization-server/db/models/access_tokens.js
+++ authorization-server/db/models/access_tokens.js
@@ -14,7 +14,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.INTEGER
     },
     scope: {
-      type: DataTypes.STRING
+      type: DataTypes.JSONB
     }
   }, {
     freezeTableName: true, // Model tableName will be the same as the model name
